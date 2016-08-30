5e74565cdd760af0e23e98d8d79b6cfc2bd237e7
Chessmasterrr <webmaster@psdatatech.de>
2016-08-30 20:43:17 +0200

    initial release

---
+++ Countdown/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Countdown
+{
+    partial class main_window
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_start = new System.Windows.Forms.Button();
+            this.label_timer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 12);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(136, 41);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_timer
+            // 
+            this.label_timer.AutoSize = true;
+            this.label_timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 150F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_timer.Location = new System.Drawing.Point(35, 80);
+            this.label_timer.Name = "label_timer";
+            this.label_timer.Size = new System.Drawing.Size(0, 226);
+            this.label_timer.TabIndex = 1;
+            this.label_timer.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_result.Location = new System.Drawing.Point(13, 70);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(0, 31);
+            this.label_result.TabIndex = 2;
+            this.label_result.Visible = false;
+            // 
+            // main_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 308);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label_timer);
+            this.Controls.Add(this.button_start);
+            this.KeyPreview = true;
+            this.Name = "main_window";
+            this.Text = "Countdown";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label_timer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ Countdown/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Countdown
+{
+    public partial class main_window : Form
+    {
+        private int counter; // overall counter
+        private int invisible; // time to get the counter invisible
+        private List<KeyValuePair<string, int>> keys = new List<KeyValuePair<string, int>>(); // list of pressed keys
+
+
+        public main_window()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Format an int to a string with dot.
+        /// </summary>
+        /// <param name="counter"></param>
+        /// <returns></returns>
+        private string formatTime(int counter)
+        {
+            return (counter / 10).ToString() + "." + (counter % 10).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button_start.Text == "Start")
+            {
+                // start countdown
+                Random rand = new Random();
+                button_start.Text = "Stopp";
+                counter = rand.Next(200) + 200; // min 20 seconds + random 20 seconds
+                invisible = rand.Next(150) + 50; // min 5 seconds + random 15 seconds
+                label_timer.Text = formatTime(counter);
+                label_timer.Visible = true;
+                label_result.Visible = false;
+                keys.Clear();
+                timer1.Start();
+            } else
+            {
+                // stop countdown and show keys
+                label_timer.Visible = false;
+                label_result.Text = "";
+                // show keys
+                foreach (KeyValuePair<string, int> pair in keys)
+                {
+                    if (pair.Value < 0)
+                    {
+                        label_result.Text += "Taste " + pair.Key + ": -" + (pair.Value *(-1) / 10).ToString() + "." + (pair.Value * (-1) % 10).ToString() + Environment.NewLine;
+                    }
+                    else {
+                        label_result.Text += "Taste " + pair.Key + ": " + (pair.Value / 10).ToString() + "." + (pair.Value % 10).ToString() + Environment.NewLine;
+                    }
+                }
+                label_result.Visible = true;
+                timer1.Stop();
+                button_start.Text = "Start";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter = counter - 1;
+            if (counter > invisible)
+            {
+                label_timer.Text = formatTime(counter);
+            } else
+            {
+                label_timer.Visible = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            keys.Add(new KeyValuePair<string, int>(e.KeyData.ToString(), counter));
+        }
+    }
+}
